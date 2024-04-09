09380029f579deb22627248e5072c61fade1924e
dir-FY24matsushita <momoka.matsushita.g090@dirbato.co.jp>
2024-04-09 12:36:43 +0900

    Employee

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -24,5 +24,18 @@ public void report (int times) {
 	public void report() {
 		report(1);
 	}
-}
 
+
+public class ObjectSample {
+
+	public static void main(String[] args) {
+		 // インスタンスの作成
+        var employee = new Employee("鈴木", "営業部", "課長", 100);
+
+        // インスタンスメソッドの呼び出し
+        employee.report();
+        employee.report(2);
+    }
+
+
+	}
