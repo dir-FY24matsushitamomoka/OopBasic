10d8e17b9d6fb2469f5c4ebd1f3cb2ba83186dfe
dir-FY24matsushita <momoka.matsushita.g090@dirbato.co.jp>
2024-04-09 10:33:25 +0900

    Employeeクラス

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,5 +1,28 @@
 package company;
 
 public class Employee {
-
+//フィールド（属性）を定義する変数の宣言
+	String name;
+	String department;
+	String position;
+	int employeeId;
+	
+//コンストラクター　クラスをインスタンスを作成するさいに呼び出されるメソッド
+public Employee (String name,String department,String position, int employeeId) {
+	this.name=name;
+	this.department=department;
+	this.position=position;
+	this.employeeId=employeeId;
+	
 }
+//報告メソッド
+public void report (int times) {
+	System.out.println(times+"回目の報告をします。役職："+position+"、名前："+name);
+	
+}
+//報告メソッド（オーバーロード）
+	public void report() {
+		report(1);
+	}
+}
+
